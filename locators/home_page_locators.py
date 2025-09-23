db578583a86a9ab5e24137e682b445c8acaab0d6
srajanjain1510 <133872715+srajanjain1510@users.noreply.github.com>
2025-09-23 22:40:43 +0530

    Add HomePageLocators class with search and login locators

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -2,3 +2,8 @@
 
 WELLNESS_MENU = "a[data-testid='top-menu-link'][href*='wellness']"
 WELLNESS_SUBMENUS = "nav[aria-label='Wellness'] ul li a, nav[aria-label='Wellness'] ul li button"
+
+class HomePageLocators:
+    SEARCH_BOX = "#searchBox"
+    SEARCH_BUTTON = "#searchButton"
+    LOGIN_LINK = "#loginLink"
