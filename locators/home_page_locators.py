c24647bb787766ad5bbb0c95f7d70766244141a3
srajanjain1510 <133872715+srajanjain1510@users.noreply.github.com>
2025-09-23 16:03:12 +0530

    Replace wellness locators with Shop menu locator

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -1,4 +1,3 @@
 # locators/home_page_locators.py
 
-WELLNESS_MENU = "a[data-testid='top-menu-link'][href*='wellness']"
-WELLNESS_SUBMENUS = "nav[aria-label='Wellness'] ul li a, nav[aria-label='Wellness'] ul li button"
+SHOP_MENU_LINK = "a[href='/en-us/shop']"
