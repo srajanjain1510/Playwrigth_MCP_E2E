fb78d1a9425c6344b6f80b5c510c5785433de02f
srajanjain1510 <133872715+srajanjain1510@users.noreply.github.com>
2025-10-13 13:50:47 +0530

    Create locators/soho_house_locators.py

---
+++ locators/soho_house_locators.py
@@ -0,0 +1,8 @@
+class SohoHouseLocators:
+    # Main navigation
+    SHOP_MENU_BUTTON = "button:has-text('Shop')"
+    
+    # Shop submenu items
+    SOHO_HOME_LINK = "a:has-text('Soho Home')"
+    COWSHED_LINK = "a:has-text('Cowshed')"
+    SOHO_SKIN_LINK = "a:has-text('Soho Skin')"
